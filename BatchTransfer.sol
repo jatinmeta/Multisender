6590e656e7f8e68a3192cd6408a7ab8afe179ef2
Jatin <jatin.singhal@metaschool.so>
2024-12-12 14:02:01 +0530

    Update BatchTransfer.sol

--- BatchTransfer.sol
+++ BatchTransfer.sol
@@ -1,10 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IERC20 {function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);}
+interface IERC20 
+{
+    // function transfer(address recipient, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+}
+
+library SafeERC20 
+{
+    function safeTransferFrom(IERC20 token,address sender,address recipient,uint256 amount) internal 
+    {
+        // Call transferFrom
+        (bool success, bytes memory data) = address(token).call(
+            abi.encodeWithSelector(token.transferFrom.selector,sender,recipient,amount)
+        );
+        require(success && (data.length == 0 || abi.decode(data, (bool))),"SafeERC20: Transfer failed");
+    }
+}
 
 contract BatchTransfer 
 {
+    using SafeERC20 for IERC20;
     function batchSendETH(address[] calldata recipients, uint256[] calldata amounts) external payable 
     {   
         require(recipients.length > 0, "No recipients provided");
@@ -26,18 +46,20 @@ contract BatchTransfer
             if (!success) { revert("ETH transfer failed");}
         }
     }
+    
 
     function batchSendERC20(address token,address[] calldata recipients,uint256[] calldata amounts) external 
-    {
+    { 
         require(token != address(0), "Invalid token address");
         require(recipients.length > 0, "No recipients provided");
         require(amounts.length > 0, "No amounts provided");
         require(recipients.length == amounts.length, "Mismatched recipients and amounts");
+
         for (uint256 i = 0; i < recipients.length; i++) 
         {
             require(recipients[i] != address(0), "Invalid recipient address");
             require(amounts[i] > 0, "Amount must be greater than 0");
-            require(IERC20(token).transferFrom(msg.sender,recipients[i], amounts[i]),"ERC-20 transfer failed");
+            IERC20(token).safeTransferFrom(msg.sender, recipients[i], amounts[i]);
         }
     }
 
